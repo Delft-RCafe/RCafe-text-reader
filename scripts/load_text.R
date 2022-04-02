88ef618bd85217a405bf395ef88c5357e9488c7a
alwil <alexandra.wilczynska@gmail.com>
2022-04-02 17:04:32 +0200

    adapted R reader to new folder structure

--- scripts/load_text.R
+++ scripts/load_text.R
@@ -7,7 +7,10 @@ library(data.table)
 
 # Load the text files -----------------------------------------------------
 
-file_list <- dir(path=here('data'))  # list of text files to be read
+# Data downloaded from surf drive:https://surfdrive.surf.nl/files/index.php/s/ZMtLtITQmE1bvYf#
+
+data_path <- here('data', 'IDE')
+file_list <- dir(path=data_path)  # list of text files to be read
 file_list_noext <-sub(".txt",'', file_list)
 
 text_list<-list() # List to store the files
@@ -15,7 +18,7 @@ text_list<-list() # List to store the files
 # Loop to load the files into a list
 for (filename in file_list ) {
   
-  text_list[filename] <- read_file(here('data', filename) )
+  text_list[filename] <- read_file(here(data_path, filename) )
 }
 
 text_data<-as.data.table(do.call(rbind, text_list))# Convert to data.table 
@@ -30,6 +33,9 @@ text_data[,`:=`(full_text_lower = tolower(full_text),
                 author_name_file = sub('[^a-zA-Z]+','', file_list_noext ),
                 copyright = str_detect( full_text, '[cC]opyright|©'  )
                 )]
+
+# Read the metadata file
+
 
 # Save the data -----------------------------------------------------------
 saveRDS(text_data, here('data_output','text_data.RDS'))
